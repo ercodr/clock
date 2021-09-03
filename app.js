d0b081dae747941fe50e7216f804fb23f042ff5b
ercodr <82173838+Dahcodingkid@users.noreply.github.com>
2021-09-03 12:18:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+let hr = document.getElementById('hr');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+// Set Interval is to create a timer
+setInterval( () => {
+
+    // Convert to 12 hour time format
+    hr.innerText = (new Date().getHours() % 12) || 12; 
+
+    min.innerText = new Date().getMinutes();
+
+    sec.innerText = new Date().getSeconds();
+
+}, 1000);
+
+
+let time = new Date().getHours();
+let greet = document.querySelector('.greet');
+
+setInterval( () => {
+
+    if(time < 12){
+
+        greet.innerHTML = 'Good Morning';
+
+    } else if(time < 17){
+
+        greet.innerHTML = 'Good Afternoon';
+
+    } else{
+
+        greet.innerHTML = 'Good Evening';
+
+    }
+
+}, 1000)
